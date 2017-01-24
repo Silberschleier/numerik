63bed4e6f56c51dff8dd701f104e5a1f64725ede
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-01-24 18:47:31 +0100

    Aufgabe 1b

--- Blatt10/aufgabe01.py
+++ Blatt10/aufgabe01.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot
+
 
 def GetQuarterFloat(sign, exponent, mantissa):
     if exponent == 0:
@@ -8,4 +10,14 @@ def GetQuarterFloat(sign, exponent, mantissa):
     return f if sign == 0 else -f
 
 if __name__ == "__main__":
-    print(GetQuarterFloat(0, 2, 3))
+    positive_quarter_floats = []
+    for e in range(0, 15):
+        positive_quarter_floats += [GetQuarterFloat(0, e, m) for m in range(0, 7)]
+
+    biggest = max(positive_quarter_floats)
+    smallest = min([x for x in positive_quarter_floats if x != 0])
+    length = len(positive_quarter_floats)
+    print("Groesste Zahl: {}, Kleinste positive Zahl: {}, Anzahl moeglicher Zahlen: {}".format(biggest, smallest, length))
+
+    pyplot.scatter(positive_quarter_floats, [0]*length)
+    pyplot.show()
